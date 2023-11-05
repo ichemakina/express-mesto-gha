524c411d095a4041ae96ab24b422a97c85c6f230
ichemakina <themackinaira@yandex.ru>
2023-11-05 18:43:17 +0300

    Рефакторинг схем. Убирает лишнюю библиотеку

--- models/user.js
+++ models/user.js
@@ -3,20 +3,20 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Поле "Имя" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "Название" - 2'],
+    maxlength: [30, 'Максимальная длина поля "Название" - 30'],
   },
   about: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Поле "О себе" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "Название" - 2'],
+    maxlength: [30, 'Максимальная длина поля "Название" - 30'],
   },
   avatar: {
     type: String,
-    required: true,
+    required: [true, 'Поле "Ссылка на новой аватар" должно быть заполнено'],
   },
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model('user', userSchema);
