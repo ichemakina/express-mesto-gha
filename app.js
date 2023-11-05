8cb7c007d474925a6ea530e5631d8c9f1e7652f0
ichemakina <themackinaira@yandex.ru>
2023-11-05 16:49:59 +0300

    Рефакторинг: изменяет подключение к бд

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
