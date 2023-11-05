f8090cd284724ce260b7cdec221df1f4fb382ed3
ichemakina <themackinaira@yandex.ru>
2023-11-05 15:32:28 +0300

    Добавляет обработку ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 const Card = require('../models/card');
 
 module.exports.getCards = router.get('/cards', (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        return res.status(400).send({ message: 'Карточки не найдены' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
 
 module.exports.createCard = router.post('/cards', (req, res) => {
@@ -12,13 +18,23 @@ module.exports.createCard = router.post('/cards', (req, res) => {
 
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        return res.status(400).send({ message: 'Введены некоректные данные' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
 
 module.exports.deleteCard = router.delete('/cards/:cardId', (req, res) => {
   Card.findByIdAndDelete(req.params.cardId)
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
 
 module.exports.likeCard = router.put('/cards/:cardId/likes', (req, res) => {
@@ -28,7 +44,15 @@ module.exports.likeCard = router.put('/cards/:cardId/likes', (req, res) => {
     { new: true },
   )
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      if (err instanceof mongoose.Error.ValidationError) {
+        return res.status(400).send({ message: 'Введены некоректные данные' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
 
 module.exports.dislikeCard = router.delete('/cards/:cardId/likes', (req, res) => {
@@ -38,5 +62,13 @@ module.exports.dislikeCard = router.delete('/cards/:cardId/likes', (req, res) =>
     { new: true },
   )
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      if (err instanceof mongoose.Error.ValidationError) {
+        return res.status(400).send({ message: 'Введены некоректные данные' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,27 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 const User = require('../models/user');
 
 module.exports.getUsers = router.get('/users', (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        return res.status(400).send({ message: 'Пользователи не найдены' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
 
 module.exports.getUser = router.get('/users/:userId', (req, res) => {
   User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        return res.status(404).send({ message: 'Пользователь не найден' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
 
 module.exports.createUser = router.post('/users', (req, res) => {
@@ -18,19 +29,40 @@ module.exports.createUser = router.post('/users', (req, res) => {
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        return res.status(400).send({ message: 'Введены некоректные данные' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
 
 module.exports.updateUser = router.patch('/users/me', (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        return res.status(404).send({ message: 'Пользователь не найден' });
+      }
+      if (err instanceof mongoose.Error.ValidationError) {
+        return res.status(400).send({ message: 'Введены некоректные данные' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
 
 module.exports.updateUserAvatar = router.patch('/users/me/avatar', (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        return res.status(404).send({ message: 'Пользователь не найден' });
+      }
+      if (err instanceof mongoose.Error.ValidationError) {
+        return res.status(400).send({ message: 'Введены некоректные данные' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
 });
