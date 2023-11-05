afd67c84e677ec3d4ec4774158b1cd51bd41c322
ichemakina <themackinaira@yandex.ru>
2023-11-05 16:34:26 +0300

    Убирает warning

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1/mestodb', {
-  useNewUrlParser: true,
-});
+mongoose.connect('mongodb://127.0.0.1/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
